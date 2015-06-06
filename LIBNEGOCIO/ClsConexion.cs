16a665ba29c8a9c2addc7fa89f580a48e8849298
xhadesxsc <leo_2085@hotmail.com>
2015-06-06 00:25:20 -0500

    todos

---
+++ LIBNEGOCIO/ClsConexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LIBNEGOCIO
+{
+    class ClsConexion
+    {
+        public static string Neptuno
+        {
+            get
+            {
+                return "Server=localhost;" +
+                    "Integrated Security=SSPI;" +
+                    "Database=Neptuno";
+            }
+        }
+    }
+}
---
+++ WebSite/Default2.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using LIBNEGOCIO;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Write("<h2><b>Numero de Pedidos " +
+                        Session["idpedidos"].ToString());
+        GridView1.DataSource = ClsAplicacion1.ProductosVendidosporPedidos(
+                                Session["idpedidos"].ToString());
+        GridView1.DataBind();
+    }
+}
